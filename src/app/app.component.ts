5ae83f0f314f59768345eb5a74a6e5c4e047dfcf
fahmidaA71 <fahmida.anwar@gmail.com>
2020-06-04 12:35:34 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app27';
+  title = ' Finally Got You Angular!!';
+  name = 'Fahmida';
 }
+
+
+// navs = [
+//   { name: 'Home',route:'/',id:100 },
+//   { name: 'About', route:'/about',id:101},
+//   { name: 'Contact', route:'/contact',id:101}
+// ];
